e2a12000de6277b8df74c83db67b39a3c3d23119
Valentina <valelore98@gmail.com>
2022-10-11 15:24:27 +0200

    Init commit

---
+++ src/com/agency/travel/Agenzia.java
@@ -0,0 +1,10 @@
+package com.agency.travel;
+
+import java.util.ArrayList;
+
+public class Agenzia {
+    public static void main(String[] args) {
+        ArrayList<String> sas = new ArrayList<>();
+
+    }
+}
---
+++ src/com/agency/travel/Vacanza.java
@@ -0,0 +1,20 @@
+package com.agency.travel;
+
+import java.util.Date;
+
+public class Vacanza {
+    String destination;
+    Date departureDay;
+    Date returnDay;
+
+    //date checks
+    protected checkNotNull(){
+
+    }
+
+    protected checkNotElapsed(){
+
+    }
+
+    protected check 
+}
