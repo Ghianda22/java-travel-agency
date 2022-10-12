973c7c669162236bea11418a37000e129f8b7505
Valentina <valelore98@gmail.com>
2022-10-12 15:44:45 +0200

    Added Agenzia class, created setData(), moved exceptions handleing into Vacanza class

--- src/com/agency/travel/Agenzia.java
+++ src/com/agency/travel/Agenzia.java
@@ -1,10 +1,57 @@
 package com.agency.travel;
 
+import com.sun.security.jgss.GSSUtil;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Agenzia {
     public static void main(String[] args) {
-        ArrayList<String> sas = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
 
+        boolean reload = true;
+
+        while(reload){
+            System.out.println("Vuoi inserire una nuova vacanza? y/n");
+            String answ = in.nextLine();
+            if(answ.equalsIgnoreCase("y")){
+                /*boolean dataError = false;
+                Vacanza holiday = null;
+                while(!dataError){
+                    System.out.println("Inserisci la destinazione:");
+                    String destination = ;
+                    System.out.println("Scegli la data di partenza:");
+                    LocalDate departureDay = setDate();
+                    System.out.println("Quando ritornerai?");
+                    LocalDate returnDay = setDate();
+                    try {
+                        holiday = new Vacanza(destination, departureDay, returnDay);
+                        dataError = false;
+                    } catch (InstantiationException e) {
+                        System.out.println(e.getMessage());
+                        dataError = true;
+                    }
+                };*/
+                Vacanza holiday = new Vacanza();
+                DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                System.out.printf(
+                        "Hai prenotato una vacanza di %d giorni a %s dal %s al %s\n",
+                        holiday.holidayDuration(),
+                        holiday.getDestination(),
+                        holiday.getDepartureDay().format(df),
+                        holiday.getReturnDay().format(df)
+                );
+            }else if(answ.equalsIgnoreCase("n")) {
+                reload = false;
+            }
+            else System.out.println("inserisci 'y' = yes o 'n' = no per fare una scelta");
+        }
     }
+
+
+
 }
--- src/com/agency/travel/Vacanza.java
+++ src/com/agency/travel/Vacanza.java
@@ -2,14 +2,17 @@ package com.agency.travel;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
 
 public class Vacanza {
-    String destination;
-    LocalDate departureDay;
-    LocalDate returnDay;
 
-    public Vacanza(String destination, LocalDate departureDay, LocalDate returnDay) throws InstantiationException {
+    private Scanner in = new Scanner(System.in);
+    private String destination = null;
+    private LocalDate departureDay = null;
+    private LocalDate returnDay = null;
+
+    /*public Vacanza(String destination, LocalDate departureDay, LocalDate returnDay) throws InstantiationException {
         try{
             checkNotNull(departureDay, returnDay);
             checkNotElapsed(departureDay);
@@ -20,10 +23,57 @@ public class Vacanza {
         }catch(NullPointerException | DateTimeException e){
             throw new InstantiationException(e.getMessage());
         }
+    }*/
 
+    public Vacanza(){
+        boolean dataError = false;
+        do {
+            try {
+                if(destination == null) setDestination();
+                if(departureDay == null) setDepartureDay();
+                if(returnDay == null) setReturnDay();
+                checkNotNull(departureDay, returnDay);
+                checkNotElapsed(departureDay);
+                checkTimeline(departureDay, returnDay);
+                dataError = false;
+            } catch (DateTimeException e) {
+                System.out.println(e.getMessage());
+                departureDay = null;
+                returnDay = null;
+                dataError = true;
+            } catch (NullPointerException e){
+                System.out.println(e.getMessage());
+                //params already null, at the loop restart they'll be assigned
+                dataError = true;
+            }
+        } while(dataError);
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+    public void setDestination() {
+        System.out.println("Inserisci la destinazione:");
+        this.destination = in.nextLine();
+    }
+    public LocalDate getDepartureDay() {
+        return departureDay;
+    }
+    public void setDepartureDay() {
+        System.out.println("Scegli la data di partenza:");
+        this.departureDay = setDate();
+    }
+    public LocalDate getReturnDay() {
+        return returnDay;
+    }
+    public void setReturnDay() {
+        System.out.println("Quando ritornerai?");
+        this.returnDay = setDate();
     }
 
     //date checks
+    /*TODO: try to call checks right into the set methods, so to have the set methods complete and ready to be called from another class
+    ex. to modify dates after a booking */
     protected void checkNotNull(LocalDate departureDay, LocalDate returnDay) throws NullPointerException{
         if(departureDay == null || returnDay == null)
             throw new NullPointerException("Devi inserire entrambe le date");
@@ -39,7 +89,27 @@ public class Vacanza {
             throw new DateTimeException("Non puoi andar via prima di arrivare!\nLa data di arrivo e' antecedente la data di partenza");
     }
 
-    public holidayDuration(){
-
+    public long holidayDuration(){
+        return ChronoUnit.DAYS.between(this.departureDay, this.returnDay);
     }
+
+    public LocalDate setDate(){
+        while(true){
+            System.out.print("Giorno:  ");
+            int day = in.nextInt();
+            in.nextLine();
+            System.out.print("  Mese:  ");
+            int month = in.nextInt();
+            in.nextLine();
+            System.out.print("  Anno:  ");
+            int year = in.nextInt();
+            in.nextLine();
+            try {
+                return LocalDate.of(year, month, day);
+            } catch (DateTimeException e) {
+                System.out.println("Inserisci dei dati validi: giorni da 1 a 31, mesi da 1 a 12");
+            }
+        }
+    }
+
 }
